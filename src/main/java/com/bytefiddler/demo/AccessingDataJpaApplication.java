59de0b8da5c633ca32ddb202d7ae23380e140b34
BuildTools <unconfigured@null.spigotmc.org>
2020-02-07 16:43:46 +0100

    basic jpa with web

---
+++ src/main/java/com/bytefiddler/demo/AccessingDataJpaApplication.java
@@ -0,0 +1,18 @@
+package com.bytefiddler.demo;
+import com.bytefiddler.demo.accessingdatajpa.Customer;
+import com.bytefiddler.demo.accessingdatajpa.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AccessingDataJpaApplication {
+    public static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataJpaApplication.class);
+    }
+}
--- src/main/java/com/bytefiddler/demo/HelloController.java
+++ src/main/java/com/bytefiddler/demo/HelloController.java
@@ -1,14 +1,65 @@
 package com.bytefiddler.demo;
 
+import com.bytefiddler.demo.accessingdatajpa.Customer;
+import com.bytefiddler.demo.accessingdatajpa.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+    private static final Logger log = AccessingDataJpaApplication.log;
+    private static final String htmlBr = "<br />";
+
+    @Autowired
+    private CustomerRepository repository;
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        System.out.println("Is Andrea happy?");
+        // save a few customers
+        repository.save(new Customer("Jack", "Bauer"));
+        repository.save(new Customer("Chloe", "O'Brian"));
+        repository.save(new Customer("Kim", "Bauer"));
+        repository.save(new Customer("David", "Palmer"));
+        repository.save(new Customer("Michelle", "Dessler"));
+
+        // fetch all customers
+        log.info("Customers found with findAll():");
+        log.info("-------------------------------");
+        for (Customer customer : repository.findAll()) {
+            log.info(customer.toString());
+        }
+        log.info("");
+
+        // fetch an individual customer by ID
+        Customer customer = repository.findById(1L);
+        log.info("Customer found with findById(1L):");
+        log.info("--------------------------------");
+        log.info(customer.toString());
+        log.info("");
+
+        // fetch customers by last name
+        log.info("Customer found with findByLastName('Bauer'):");
+        log.info("--------------------------------------------");
+        repository.findByLastName("Bauer").forEach(bauer -> {
+            log.info(bauer.toString());
+        });
+        // for (Customer bauer : repository.findByLastName("Bauer")) {
+        //  log.info(bauer.toString());
+        // }
+        log.info("");
+
+        String result = "";
+        for ( Customer cust : repository.findAll()) {
+            result += htmlBr + cust.toString();
+        }
+
+        return "Greetings from Spring Boot!" + htmlBr + result;
     }
 
 }
