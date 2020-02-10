e26c4c081b930d9725be7fece1cd04b06e7550c6
Wuodan <stefan.kuhn.zurich@gmail.com>
2020-02-10 21:28:00 +0100

    Split HelloWorld and JPA Demo

--- src/main/java/com/bytefiddler/demo/accessingdatajpa/DemoController.java
+++ src/main/java/com/bytefiddler/demo/accessingdatajpa/DemoController.java
@@ -1,26 +1,21 @@
-package com.bytefiddler.demo;
+package com.bytefiddler.demo.accessingdatajpa;
 
-import com.bytefiddler.demo.accessingdatajpa.Customer;
-import com.bytefiddler.demo.accessingdatajpa.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
-    public static final Logger log = LoggerFactory.getLogger(HelloController.class);
+public class DemoController {
+    public static final Logger log = LoggerFactory.getLogger(DemoController.class);
     private static final String htmlBr = "<br />";
 
     @Autowired
     private CustomerRepository repository;
 
-    @RequestMapping("/")
-    public String index() {
-        System.out.println("Is Andrea happy?");
+    @RequestMapping("/accessingdatajpa")
+    public String accessingdatajpa() {
         // save a few customers
         repository.save(new Customer("Jack", "Bauer"));
         repository.save(new Customer("Chloe", "O'Brian"));
