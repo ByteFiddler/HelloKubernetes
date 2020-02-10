42de4bb97b136568bc640859cd95c4741c9451f3
Wuodan <stefan.kuhn.zurich@gmail.com>
2020-02-10 11:10:29 +0100

    Fix container internal test

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+echo 'START: run_tests.sh'
+
+CURL_OUT="$(curl -s localhost:8080)" || exit $?
+
+echo "START: Result of 'curl -s localhost:8080':"
+echo "${CURL_OUT}"
+echo "END: Result of 'curl -s localhost:8080'"
+echo 'END: run_tests.sh: before exitting with final test'
+
 curl -s localhost:8080 | grep -q Dessler
 exit $?
